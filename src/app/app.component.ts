d1846be7cef24a8dbedc8225f55e1767c5a30cfb
pritikumar1 <priti.kumari@kelltontech.com>
2020-09-15 10:54:44 +0530

    add spaceX application code

---
+++ src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-starter';
+  version = 'Angular version 10.0.14';
+
+  dataList: any;
+
+  currentSucc = '';
+  current = 1;
+  features = [
+    2006, 2007,2008,2009, 2010,2011,2012,2013,2014
+  ];
+
+  link = 'https://api.spacexdata.com/v3/launches?limit=100';
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getListing(this.link);
+  }
+
+  getListing(link) {
+    this.http.get(link).subscribe(a => {
+      this.dataList = a
+    })
+  }
+
+  changeItem(item: any) {
+
+    this.current = item;
+  
+    if(this.link.indexOf('launch_year') <= -1) {
+      this.link = this.link+'&launch_year='+item
+    } else {
+      this.link = this.link.split('&').map(a => {
+        const l = a.split('=');
+        if(l[0] === 'launch_year') {
+          l[1] = item;
+        }
+        return l.join('=');
+      }).join('&');
+    }
+
+    this.getListing(this.link);
+  }
+
+  changeSuccess(item: any) {
+
+    this.currentSucc = item;
+
+    if(this.link.indexOf('launch_success') <= -1) {
+      this.link = this.link+'&launch_success='+item
+    } else {
+      this.link = this.link.split('&').map(a => {
+        const l = a.split('=');
+        if(l[0] === 'launch_success') {
+          l[1] = item;
+        }
+        return l.join('=');
+      }).join('&');
+    }
+
+    this.getListing(this.link);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/general/home/home.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
